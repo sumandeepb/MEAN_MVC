b8506f30d69f52607febdbcf02e160f9a833bf03
sumandeepb <sumandeep.banerjee@capillarytech.com>
2017-09-26 06:54:53 +0530

    WIP: Angular Tutorial

---
+++ public/application.js
@@ -0,0 +1,11 @@
+/* 
+ * MEAN MVC Learning Project
+ * Released under MIT License
+ */
+
+var appName = 'mean';
+var app = angular.module(appName, []);
+
+angular.element(document).ready(function() {
+    angular.bootstrap(document, [appName]);
+});
